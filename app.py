250ccb7c75faeb38c9364624d27dfd89c89cfdc7
khamidn <nurkhamid014@gmail.com>
2020-11-22 19:27:28 +0700

    add sign up & sign in

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+
 from resources.messages import messages_api
+from resources.users import users_api
+
 
 import models
 
 app = Flask(__name__)
 app.register_blueprint(messages_api, url_prefix = '/api/v1')
-# api = Api(app)
-
-
-
-# api.add_resource(messages.MessageList, '/messages')
+app.register_blueprint(users_api, url_prefix = '/api/v1')
 
 if __name__ == '__main__':
 	models.initialize()
