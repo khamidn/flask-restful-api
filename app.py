9e2ac0d8d2b2d87540295d4e5d6812410cf3dc77
khamidn <nurkhamid014@gmail.com>
2020-11-22 21:46:01 +0700

    tambahkan logout revoke

--- app.py
+++ app.py
@@ -4,10 +4,9 @@ from flask_restful import Resource, Api
 from resources.messages import messages_api
 from resources.users import users_api
 
-
 from flask_jwt_extended import (JWTManager, jwt_required, 
-								create_access_token, get_jwt_identity)
-
+								create_access_token, get_jwt_identity,
+								get_raw_jwt)
 
 import models
 
@@ -17,7 +16,25 @@ app.register_blueprint(users_api, url_prefix = '/api/v1')
 
 # ACCESS_TOKEN_JWT
 app.config['SECRET_KEY'] = 'randomString_superSecret1928390123'
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
 jwt = JWTManager(app)
+
+#logout
+blacklist = set()
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(descrypted_token):
+	jti = descrypted_token['jti']
+	return jti in blacklist
+
+@app.route('/api/v1/user/logout')
+@jwt_required
+def logout():
+	jti = get_raw_jwt()['jti']
+	blacklist.add(jti)
+	return {'msg' : 'berhasil logout'}
 
 if __name__ == '__main__':
 	models.initialize()
